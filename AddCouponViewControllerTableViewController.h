d7ff6f150c635ff2c14a39cd9d9b4ba02f503ae3
Ryan Mottley <ligerzero459@gmail.com>
2014-10-30 23:36:19 -0500

    Added viewDidLoad to AddTripViewController

---
+++ AddCouponViewControllerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddCouponViewControllerTableViewController.h
+//  Coupon Holder
+//
+//  Created by Ryan Mottley on 10/30/14.
+//  Copyright (c) 2014 Kai Strife Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddCouponViewControllerTableViewController : UITableViewController
+
+@end
--- Coupon Holder/AddTripViewController.m
+++ Coupon Holder/AddTripViewController.m
@@ -13,6 +13,21 @@
 
 @implementation AddTripViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
 - (IBAction)dismissModal:(id)sender
 {
     [self dismissViewControllerAnimated:YES completion:nil];
